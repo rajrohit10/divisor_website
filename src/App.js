d942e7e8cf546b8118cf0fc0d4c840e1ae1cfb77
rajrohit10 <rohit.rajsurya10@gmail.com>
2021-05-16 10:41:52 +0530

    creating routes  [Home-Page]

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import FillerSection from "./components/FillerSection";
-import AboutUs from "./components/AboutUs";
-import LearningModel from "./components/LearningModel";
-import Pointers from "./components/Pointers";
-import PopularMini from "./components/PopularMini"
-import Footer from "./components/Footer";
-import Filler2 from "./components/Filler2";
-import Filler3 from "./components/Filler3";
-import Testimonials from "./components/Testimonials";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/index";
 
 function App() {
   return (
     <div className="App">
-      <div className="first-section">
-        <div className="container">
-          <Navbar />
-          <Header />
-        </div>
-      </div>
-      <div className="container">
-        <FillerSection />
-        <AboutUs />
-        
-        </div>
-        <Filler3/>
-        <div className="container">
-        <LearningModel />
-        <Pointers />
-        <PopularMini/>
-       
-      </div>
-      <Filler2/>
-      <div className="container">
-        <Testimonials/>
-
-      </div>
-      <Footer/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,42 @@
+import Navbar from "./../../components/Navbar";
+import Header from "./../../components/Header";
+import FillerSection from "./../../components/FillerSection";
+import AboutUs from "./../../components/AboutUs";
+import LearningModel from "./../../components/LearningModel";
+import Pointers from "./../../components/Pointers";
+import PopularMini from "./../../components/PopularMini"
+import Footer from "./../../components/Footer";
+import Filler2 from "./../../components/Filler2";
+import Filler3 from "./../../components/Filler3";
+import Testimonials from "./../../components/Testimonials";
+
+
+const HomePage=()=>{
+    return <div>
+          <div className="first-section">
+        <div className="container">
+          <Navbar />
+          <Header />
+        </div>
+      </div>
+      <div className="container">
+        <FillerSection />
+        <AboutUs />
+        
+        </div>
+        <Filler3/>
+        <div className="container">
+        <LearningModel />
+        <Pointers />
+        <PopularMini/>
+       
+      </div>
+      <Filler2/>
+      <div className="container">
+        <Testimonials/>
+
+      </div>
+      <Footer/>
+    </div>
+}
+export default HomePage;
